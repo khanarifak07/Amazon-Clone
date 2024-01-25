376274c039870e5ee73ae10149ebd15e1ff90572
Arif Khan <khanarifak07@gmail.com>
2024-01-25 18:16:24 +0530

    Backend Setup Complete

---
+++ Backend/src/app.js
@@ -0,0 +1,33 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+const app = express();
+
+//cors configuratin as middleware (cross origin resourse sharing)
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+//configuration for json (receives data from json)
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+//configuration for url encoded via express (receives data from url)
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+//configration for asset via express e.g pdf, jpg, png
+app.use(express.static("public"));
+
+//configuration for cookies
+app.use(cookieParser());
